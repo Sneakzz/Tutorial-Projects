fdd3dad2853f43d1a76e67d8efdf5e4c32a98f41
Sneakzz <kenny.veys@gmail.com>
2019-03-08 00:28:50 +0100

    Add completed numbers-quickstart exercise

---
+++ C#/numbers-quickstart/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            //WorkingWithIntegers();
+
+            //OrderPrecedence();
+
+            //TestLimits();
+
+            //WorkWithDoubles();
+
+            //WorkingWithDecimal();
+
+            CalculateAreaOfCircle();
+        }
+
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void OrderPrecedence() {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            // demonstration that calculations done on ints will never produce
+            // a decimal
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e + f) / g;
+            Console.WriteLine(h);
+        }
+
+        static void TestLimits() {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            // min and max values of a regular integer
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            // example of overflow
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+
+        static void WorkWithDoubles() {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e + f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"the range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+        }
+
+        static void WorkingWithDecimal() {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+            
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+        }
+
+        static void CalculateAreaOfCircle() {
+            double radius = 2.50;
+            double circleArea = (radius * radius) * Math.PI;
+            
+            Console.WriteLine($"Area of a circle with radius of 2.50CM: {circleArea}");
+        }
+    }
+}
