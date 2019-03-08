8ee150af840eafcec27b51808b71304054007c88
Sneakzz <kenny.veys@gmail.com>
2019-03-08 01:35:26 +0100

    Add folder + completed interpolated exercise

---
+++ C#/interpolated/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Example
+{
+   public static void Main()
+   {
+      Console.WriteLine($"[{DateTime.Now,-20:d}] Hour [{DateTime.Now,-10:HH}] [{1063.342,15:N2}] feet");
+   }
+}
