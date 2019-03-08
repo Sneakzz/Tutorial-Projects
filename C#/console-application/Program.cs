8fedbbe7838ede85013878e32cd2b175a0bec349
Sneakzz <kenny.veys@gmail.com>
2019-03-08 04:48:37 +0100

    Add code for reading + outputting file contents with delay

--- C#/console-application/Program.cs
+++ C#/console-application/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace console_application
+namespace TeleprompterConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = ReadFrom("sampleQuotes.txt");
+
+            foreach (var line in lines) {
+                Console.WriteLine(line);
+
+                if (!string.IsNullOrWhiteSpace(line)) {
+                    var pause = Task.Delay(200);
+                    // Synchronously waiting on a task is an anti-pattern.
+                    // This will get fixed in later steps.
+                    pause.Wait();
+                }
+            }
+        }
+
+        // this is called an Iterator method
+        // Enumerator methods return sequences that are evaluated lazily.
+        static IEnumerable<string> ReadFrom(string file) {
+            string line;
+
+            // reader receives StreamReader object from the .OpenText(String) method
+            using (var reader = File.OpenText(file)) {
+
+                while ((line = reader.ReadLine()) != null) {
+                    var words = line.Split(' ');
+                    var lineLength = 0;
+
+                    foreach (var word in words) {
+                        yield return word + " ";
+                        lineLength += word.Length + 1;
+
+                        if (lineLength > 70) {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+                    }
+
+                    yield return Environment.NewLine;
+                }
+            }
         }
     }
 }
