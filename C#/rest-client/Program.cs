1be016b5754d56de0260ad63487e3a31fe9a0e5f
Sneakzz <kenny.veys@gmail.com>
2019-03-09 01:41:02 +0100

    Add folder + initial setup for REST client project

---
+++ C#/rest-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rest_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
