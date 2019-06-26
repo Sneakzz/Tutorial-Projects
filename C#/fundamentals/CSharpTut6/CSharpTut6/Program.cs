5ab446c66779da0d6ec1c965bba9ee813d098746
Sneakzz <kenny.veys@gmail.com>
2019-06-26 19:03:11 +0200

    Add part 6 of C# fundamentals

---
+++ C#/fundamentals/CSharpTut6/CSharpTut6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTut6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+            Animal cat2 = new Animal("Whisky", "Meow");
+
+            // Call the setter
+            cat.SetName("Whiskers");
+
+            // Set the property
+            cat.Sound = "Meow";
+
+            Console.WriteLine("The cat is named {0} and says {1}",
+                cat.GetName(), cat.Sound);
+
+            // Test auto generated getters and setters
+            cat.Owner = "Kenny";
+
+            Console.WriteLine("{0} owner is {1}",
+                cat.GetName(), cat.Owner);
+
+            // Get the read-only id number
+            Console.WriteLine("{0} shelter id is {1}",
+                cat.GetName(), cat.idNum);
+            Console.WriteLine("{0} shelter id is {1}",
+                cat2.GetName(), cat2.idNum);
+
+            // Test static property
+            Console.WriteLine("# of Animals : {0}",
+                Animal.NumOfAnimals);
+
+            Console.ReadLine();
+        }
+    }
+}
