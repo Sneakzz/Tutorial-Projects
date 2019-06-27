02fea11a0c5d58669918170773a0a210c9027cae
Sneakzz <kenny.veys@gmail.com>
2019-06-27 16:34:02 +0200

    Add part 8 of C# fundamentals

---
+++ C#/fundamentals/CSharpTut8/CSharpTut8/Battle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTut8
+{
+    class Battle
+    {
+        // This is a utility class so it makes sense to have just static methods
+
+        // Receive both Warrior objects
+        public static void StartFight(Warrior warrior1, Warrior warrior2)
+        {
+            // Use a loop giving each Warrior a chance to attack and block
+            // each  turn until 1 dies
+            while (true)
+            {
+                if (GetAttackResult(warrior1, warrior2) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+                
+                if (GetAttackResult(warrior2, warrior1) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+        }
+
+        // Accept 2 Warriors
+        public static string GetAttackResult(Warrior warriorA, Warrior warriorB)
+        {
+            // Calculate one Warriors attack and the others block
+            double warAAttkAmt = warriorA.Attack();
+            double warBBlckAmt = warriorB.Block();
+
+            // Subtract block from attack
+            double dmg2WarB = warAAttkAmt - warBBlckAmt;
+
+            // If there was damage, subtract that from the health
+            if (dmg2WarB > 0)
+            {
+                warriorB.Health = warriorB.Health - dmg2WarB;
+            }
+            else dmg2WarB = 0;
+
+            // Print out info on who attacked who and for how much damage
+            Console.WriteLine($"{warriorA.Name} Attacks {warriorB.Name} and Deals {dmg2WarB} Damage");
+
+            // Provide output on the change to health
+            Console.WriteLine($"{warriorB.Name} Has {warriorB.Health} Health\n");
+
+            // Check if the warriors health fell below zero and if so,
+            // print a message and send a response that will end the loop.
+            if (warriorB.Health <= 0)
+            {
+                Console.WriteLine($"{warriorB.Name} has Died and {warriorA.Name} is Victorious\n");
+                return "Game Over";
+            }
+            else return "Fight Again";
+        }
+    }
+}
---
+++ C#/fundamentals/CSharpTut8/CSharpTut8/Warrior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTut8
+{
+    class Warrior
+    {
+        // Define the Warrior properties
+        public string Name { get; set; } = "Warrior";
+        public double Health { get; set; } = 0;
+        public double AttkMax { get; set; } = 0;
+        public double BlockMax { get; set; } = 0;
+
+        // Always create a single Random instance and reuse it
+        // or you will get the same value over and over
+        Random rnd = new Random();
+
+        // Constructor initializes the warrior
+        public Warrior(string name = "Warrior", double health = 0, double attkMax = 0, double blockMax = 0)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.AttkMax = attkMax;
+            this.BlockMax = blockMax;
+        }
+
+        // Generate a random attack value from 1 to the warriors maximum attack value
+        public double Attack()
+        {
+            return this.rnd.Next(1, (int)this.AttkMax);
+        }
+
+        // Generate a random block value from 1 to the warriors maximum block value
+        public double Block()
+        {
+            return this.rnd.Next(1, (int)this.BlockMax);
+        }
+    }
+}
