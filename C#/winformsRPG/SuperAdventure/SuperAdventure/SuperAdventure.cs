5cedbc9c207f0629058efb17b936bfc862f0e560
Sneakzz <kenny.veys@gmail.com>
2019-07-27 22:28:05 +0200

    Add saving and loading player state information

--- C#/winformsRPG/SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ C#/winformsRPG/SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -11,22 +11,30 @@ using System.Windows.Forms;
 // Add the using Engine statement so this class knows where the Player class is located.
 using Engine;
 
+using System.IO;
+
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
         private Player _player;
         private Monster _currentMonster;
+        private const string PLAYER_DATA_FILE_NAME = "PlayerData.xml";
 
         public SuperAdventure()
         {
             InitializeComponent();
 
-            _player = new Player(10, 10, 20, 0);
-            MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
-            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
-            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_OP_TESTWEAPON), 1));
-            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_HEALING_POTION), 1));
+            if (File.Exists(PLAYER_DATA_FILE_NAME))
+            {
+                _player = Player.CreatePlayerFromXmlString(File.ReadAllText(PLAYER_DATA_FILE_NAME));
+            }
+            else
+            {
+                _player = Player.CreateDefaultPlayer();
+            }
+
+            MoveTo(_player.CurrentLocation);
 
             UpdatePlayerStats();
         }
@@ -498,6 +506,11 @@ namespace SuperAdventure
         {
             rtbMessages.SelectionStart = rtbMessages.Text.Length;
             rtbMessages.ScrollToCaret();
+        }
+
+        private void SuperAdventure_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(PLAYER_DATA_FILE_NAME, _player.ToXmlString());
         }
     }
 }
