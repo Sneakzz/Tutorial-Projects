ffbf47b876a9cf65165b2699483b4bce0a6a3516
Sneakzz <kenny.veys@gmail.com>
2019-07-31 00:04:50 +0200

    Add Vendor class and add Vendor to a location

---
+++ C#/winformsRPG/SuperAdventure/Engine/Vendor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Engine
+{
+    public class Vendor : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public BindingList<InventoryItem> Inventory { get; private set; }
+
+        public Vendor(string name)
+        {
+            Name = name;
+            Inventory = new BindingList<InventoryItem>();
+        }
+
+        public void AddItemToInventory(Item itemToAdd, int quantity = 1)
+        {
+            InventoryItem item = Inventory.SingleOrDefault(ii => ii.Details.ID == itemToAdd.ID);
+
+            if (item == null)
+            {
+                // The item is not yet in the inventory
+                Inventory.Add(new InventoryItem(itemToAdd, quantity));
+            }
+            else
+            {
+                // The item is allready in the inventory
+                item.Quantity += quantity;
+            }
+
+            OnPropertyChanged("Inventory");
+        }
+
+        public void RemoveItemFromInventory(Item itemToRemove, int quantity = 1)
+        {
+            InventoryItem item = Inventory.SingleOrDefault(ii => ii.Details.ID == itemToRemove.ID);
+
+            if (item == null)
+            {
+                // The item is not in the inventory, maybe raise error for this situation
+            }
+            else
+            {
+                // The item is in the inventory, so decrease it's quantity
+                item.Quantity -= quantity;
+
+                // Don't allow negative quantities.
+                if (item.Quantity < 0)
+                {
+                    item.Quantity = 0;
+                }
+
+                // If the quantity is zero, remove the item from the list
+                if (item.Quantity == 0)
+                {
+                    Inventory.Remove(item);
+                }
+
+                // Notify the UI that the inventory has changed
+                OnPropertyChanged("Inventory");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
