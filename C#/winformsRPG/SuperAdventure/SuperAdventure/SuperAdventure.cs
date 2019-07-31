cb7a16540b5412ea513308d492f538f177467784
Sneakzz <kenny.veys@gmail.com>
2019-07-31 04:06:13 +0200

    Add db connection for loading/saving the game

--- C#/winformsRPG/SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ C#/winformsRPG/SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -25,14 +25,29 @@ namespace SuperAdventure
         {
             InitializeComponent();
 
-            if (File.Exists(PLAYER_DATA_FILE_NAME))
+            _player = PlayerDataMapper.CreateFromDatabase();
+
+            if (_player == null)
             {
-                _player = Player.CreatePlayerFromXmlString(File.ReadAllText(PLAYER_DATA_FILE_NAME));
+                if (File.Exists(PLAYER_DATA_FILE_NAME))
+                {
+                    _player = Player.CreatePlayerFromXmlString(File.ReadAllText(PLAYER_DATA_FILE_NAME));
+                }
+                else
+                {
+                    _player = Player.CreateDefaultPlayer();
+                }
             }
-            else
-            {
-                _player = Player.CreateDefaultPlayer();
-            }
+
+            // Load/Save XML Version
+            //if (File.Exists(PLAYER_DATA_FILE_NAME))
+            //{
+            //    _player = Player.CreatePlayerFromXmlString(File.ReadAllText(PLAYER_DATA_FILE_NAME));
+            //}
+            //else
+            //{
+            //    _player = Player.CreateDefaultPlayer();
+            //}
 
             // connect the text property of lblHitPoints to the CurrentHitPoints property of the _player object
             lblHitPoints.DataBindings.Add("Text", _player, "CurrentHitPoints");
@@ -204,6 +219,8 @@ namespace SuperAdventure
         private void SuperAdventure_FormClosing(object sender, FormClosingEventArgs e)
         {
             File.WriteAllText(PLAYER_DATA_FILE_NAME, _player.ToXmlString());
+
+            PlayerDataMapper.SaveToDatabase(_player);
         }
 
         private void cboWeapons_SelectedIndexChanged(object sender, EventArgs e)
