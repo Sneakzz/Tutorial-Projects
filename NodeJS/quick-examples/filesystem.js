f10de419641555c1b97f955208541e4bfd424013
Sneakzz <kenny.veys@gmail.com>
2019-08-26 22:38:55 +0200

    Modify example to opening, truncating, reading and closing a file

--- NodeJS/quick-examples/filesystem.js
+++ NodeJS/quick-examples/filesystem.js
@@ -1,4 +1,4 @@
-/* Closing a File */
+/* Truncate a File */
 
 const fs = require('fs');
 const buf = new Buffer(1024);
@@ -9,21 +9,27 @@ fs.open('input.txt', 'r+', (err, fd) => {
   if (err) return console.error(err);
 
   console.log('File opened successfully!');
-  console.log('Going to read the file');
+  console.log('Going to truncate the file after 10 bytes');
 
-  fs.read(fd, buf, 0, buf.length, 0, (err, bytes) => {
+  // Truncate the opened file.
+  fs.ftruncate(fd, 10, err => {
     if (err) return console.error(err);
 
-    // Print only read bytes to avoid junk.
-    if (bytes > 0) {
-      console.log(buf.slice(0, bytes).toString());
-    }
+    console.log('File truncated successfully');
+    console.log('Going to read the same file');
 
-    // Close the opened file.
-    fs.close(fd, err => {
+    fs.read(fd, buf, 0, buf.length, 0, (err, bytes) => {
       if (err) return console.error(err);
 
-      console.log('File closed successfully.');
+      // Print only read bytes to avoid junk.
+      if (bytes > 0) console.log(buf.slice(0, bytes).toString());
+
+      // Close the opened file
+      fs.close(fd, err => {
+        if (err) return console.error(err);
+
+        console.log('File closed successfully!');
+      });
     });
   });
 });
