d87b8cf44ebd6b05114fcd7aff94d4e9861fa77f
Sneakzz <kenny.veys@gmail.com>
2019-08-27 15:45:17 +0200

    Modify code to a very simple example of getting cookies from client

--- NodeJS/quick-examples/express.js
+++ NodeJS/quick-examples/express.js
@@ -1,48 +1,14 @@
-/* File Upload */
+/* Cookies Management */
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const app = express();
-const fs = require('fs');
+app.use(cookieParser());
 
-const bodyParser = require('body-parser');
-const multer = require('multer');
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(multer({dest: './tmp/'}).single('file'));
-
-app.get('/index.html', (req, res) => {
-  res.sendFile(__dirname + '/' + 'index.html');
+app.get('/', (req, res) => {
+  console.log('Cookies: ', req.cookies);
+  res.send(req.cookies);
 });
 
-app.post('/file_upload', (req, res) => {
-  console.log(req.file.originalname);
-  console.log(req.file.path);
-  console.log(req.file.mimetype);
-  console.log(req.file.encoding);
-  const file = __dirname + '/' + req.file.originalname;
-  console.log(file);
-
-  fs.readFile(req.file.path, (err, data) => {
-    fs.writeFile(file, data, err => {
-      if (err) {
-        console.log(err);
-      } else {
-        response = {
-          message: 'File uploaded successfully',
-          filename: req.file.originalname
-        };
-      }
-      
-      console.log(response);
-      res.end(JSON.stringify(response));
-    });
-  });
-});
-
-const server = app.listen(8081, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log(`Example app listening at http://${host}:${port}`);
-});
+app.listen(8081);
