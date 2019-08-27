137427a0862b9553567eb40ce6b007928b88062b
Sneakzz <kenny.veys@gmail.com>
2019-08-27 18:46:35 +0200

    Modify code to example of RESTful post request

--- NodeJS/quick-examples/RESTful.js
+++ NodeJS/quick-examples/RESTful.js
@@ -2,12 +2,33 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+const user = {
+  'user4' : {
+    'name' : 'mohit',
+    'password' : 'password4',
+    'profession' : 'teacher',
+    'id' : 4
+  }
+};
+
 app.get('/listUsers', (req, res) => {
   fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
     if (err) return console.error(err);
 
     console.log(data);
     res.end(data);
+  });
+});
+
+app.post('/addUser', (req, res) => {
+  // First read existing users.
+  fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
+    if (err) return console.error(err);
+
+    data = JSON.parse(data);
+    data['user4'] = user['user4'];
+    console.log(data);
+    res.end(JSON.stringify(data));
   });
 });
 
