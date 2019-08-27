bce164e92a458194a3335e0e46ca95f555f87f24
Sneakzz <kenny.veys@gmail.com>
2019-08-27 14:14:05 +0200

    Add images and modify code to an example of serving static files with express

--- NodeJS/quick-examples/express.js
+++ NodeJS/quick-examples/express.js
@@ -1,36 +1,12 @@
-/* Basic routing */
+/* Serving Static Files */
 
 const express = require('express');
 const app = express();
 
-// This responds with 'Hello GET' on the homepage
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  console.log('Got a GET request for the homepage');
-  res.send('Hello GET');
-});
-
-// This responds to a POST request for the homepage
-app.post('/', (req, res) => {
-  console.log('Got a POST request for the homepage');
-  res.send('Hello POST');
-});
-
-// This responds to a DELETE request for the /del_user page.
-app.delete('/del_user', (req, res) => {
-  console.log('Got a DELETE request for /del_user');
-  res.send('Hello DELETE');
-});
-
-// This responds to a GET request for the /list_user page.
-app.get('/list_user', (req, res) => {
-  console.log('Got a GET request for /list_user');
-  res.send('Page Listing');
-});
-
-// This responds to a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', (req, res) => {
-  console.log('Got a GET request for /ab*cd');
-  res.send('Page Pattern Match');
+  res.send('Hello World');
 });
 
 const server = app.listen(8081, () => {
@@ -38,4 +14,4 @@ const server = app.listen(8081, () => {
   const port = server.address().port;
 
   console.log(`Example app listening at http://${host}:${port}`);
-})
+});
