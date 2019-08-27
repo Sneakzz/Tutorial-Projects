85e4f79a12687e7a41f4ee8fa50cd819ea54b061
Sneakzz <kenny.veys@gmail.com>
2019-08-27 02:26:33 +0200

    Modify code to add example of a web client

--- NodeJS/quick-examples/webModule.js
+++ NodeJS/quick-examples/webModule.js
@@ -1,39 +1,45 @@
-/* Creating a Web Server using Node */
-
-const http = require('http');
-const fs = require('fs');
 const url = require('url');
+const fs = require('fs');
+const http = require('http');
 
-// Create a server
 http.createServer( (req, res) => {
-  // Parse the request containing file name
   const pathname = "." + url.parse(req.url).pathname;
 
-  // Print the name of the file for which request is made.
-  console.log(`Request for ${pathname} received.`);
-
-  // Read the requested file content from file system
   fs.readFile(pathname, (err, data) => {
     if (err) {
-      console.log(err);
-
-      // HTTP Status: 404 : NOT FOUND
-      // Content Type: text/html
       res.writeHead(404, {'Content-Type':'text/html'});
-      res.write('error occurred');
     } else {
-      // Page found
-      // HTTP Status: 200 : OK
-      // Content Type: text/html
       res.writeHead(200, {'Content-Type':'text/html'});
-
-      // Write the content of the file to response body
       res.write(data.toString());
     }
 
-    // Send the response body
     res.end();
   });
 }).listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/');
+/* Creating a Web Client using Node */
+
+// Options to be used by request
+const options = {
+  host: 'localhost',
+  port: '8081',
+  path: '/index.html'
+};
+
+// Callback function is used to deal with response
+const callback = response => {
+  // Continuously update stream with data
+  let body = '';
+  response.on('data', data => {
+    body += data;
+  });
+
+  response.on('end', () => {
+    // Data received completely.
+    console.log(body);
+  });
+}
+
+// Make a request to the server
+const req = http.request(options, callback);
+req.end();
