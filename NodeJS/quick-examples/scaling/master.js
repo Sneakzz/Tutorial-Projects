35a86116deb3ee0a62aa8978ef80f952e50337fc
Sneakzz <kenny.veys@gmail.com>
2019-08-28 14:12:08 +0200

    Modify code to another example of the child_process module and the spawn method

--- NodeJS/quick-examples/scaling/master.js
+++ NodeJS/quick-examples/scaling/master.js
@@ -1,23 +1,22 @@
 /* child_process module */
 
-// The exec() method
-// Runs a command in a shell and buffers the output.
+// The spawn() method
+// launches a new process with given command.
 const fs = require('fs');
 const child_process = require('child_process');
 
 for(let i = 0; i < 3; i++) {
-  const workerProcess = child_process.exec('node scaling/support.js ' + i, (error, stdout, stderr) => {
-    if (error) {
-      console.log(error.stack);
-      console.log(`Error code: ${error.code}`);
-      console.log(`Signal received: ${error.signal}`);
-    }
+  const workerProcess = child_process.spawn('node', ['scaling/support.js', i]);
 
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr ${stderr}`);
+  workerProcess.stdout.on('data', data => {
+    console.log(`stdout: ${data}`);
   });
 
-  workerProcess.on('exit', code => {
+  workerProcess.stderr.on('data', data => {
+    console.log(`stderr: ${data}`);
+  });
+
+  workerProcess.on('close', code => {
     console.log(`Child process exited with exit code ${code}`);
   });
 }
