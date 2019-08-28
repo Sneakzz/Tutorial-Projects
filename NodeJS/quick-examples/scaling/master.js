60894afe75de2b6f2973cea48b1d83c43d5959c4
Sneakzz <kenny.veys@gmail.com>
2019-08-28 14:17:17 +0200

    Modify code to another example of the child_process module and the fork method

--- NodeJS/quick-examples/scaling/master.js
+++ NodeJS/quick-examples/scaling/master.js
@@ -1,22 +1,14 @@
 /* child_process module */
 
-// The spawn() method
-// launches a new process with given command.
+// The fork() method
+// This is a special case of the spawn method() to create Node processes
 const fs = require('fs');
 const child_process = require('child_process');
 
 for(let i = 0; i < 3; i++) {
-  const workerProcess = child_process.spawn('node', ['scaling/support.js', i]);
-
-  workerProcess.stdout.on('data', data => {
-    console.log(`stdout: ${data}`);
-  });
-
-  workerProcess.stderr.on('data', data => {
-    console.log(`stderr: ${data}`);
-  });
+  const workerProcess = child_process.fork('scaling/support.js', [i]);
 
   workerProcess.on('close', code => {
-    console.log(`Child process exited with exit code ${code}`);
+    console.log(`Child process exited with code ${code}`);
   });
 }
