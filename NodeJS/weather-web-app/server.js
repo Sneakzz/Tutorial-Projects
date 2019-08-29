eb8e39af703b38893540f693b78f1d22ba9f55ef
Sneakzz <kenny.veys@gmail.com>
2019-08-29 22:01:24 +0200

    Add request module + code to handle api request and response

--- NodeJS/weather-web-app/server.js
+++ NodeJS/weather-web-app/server.js
@@ -1,9 +1,36 @@
 const express = require('express');
-app.set('view engine', 'ejs');
+// body-parser module is used so the req.body object can be accessed.
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
+const apiKey = '27783d2cd441a3de408803ff6ceccd2d';
+
+// allows access to all static files within the 'public' folder
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/', (req, res) => {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  request(url, (err, res, body) => {
+    if (err) {
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      if (weather.main == undefined) {
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 });
 
 app.listen(3000, () => {
