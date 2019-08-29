5150c4df0c24da17a270b0dcb89d54c20981d9a5
Sneakzz <kenny.veys@gmail.com>
2019-08-29 16:10:14 +0200

    Add functionality to set variables from cli

--- NodeJS/weather-app/index.js
+++ NodeJS/weather-app/index.js
@@ -1,8 +1,10 @@
 // Node module called request, simplifies the code needed to make an http request.
 const request = require('request');
+// set up yargs, allows for setting up variables from the command line
+const argv = require('yargs').argv;
 
 let apiKey = '27783d2cd441a3de408803ff6ceccd2d';
-let city = 'Brugge';
+let city = argv.c || 'Brugge';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 request(url, (err, res, body) => {
