8d645a74ad1f0487ce0970033f82ebd14ad138a4
Sneakzz <kenny.veys@gmail.com>
2019-09-03 20:59:46 +0200

    Add middleware and code for error handling

--- NodeJS/restApi/index.js
+++ NodeJS/restApi/index.js
@@ -14,6 +14,13 @@ app.use(bodyParser.json());
 // Initialize routes
 app.use('/api', require('./routes/api'));
 
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  // console.log(err);
+  res.status(422).send({error: err._message});
+});
+
 // Listen for requests
 app.listen(process.env.PORT || 4000, () => {
   console.log('Now listening for requests');
--- NodeJS/restApi/routes/api.js
+++ NodeJS/restApi/routes/api.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 // Get list of ninjas from db
-router.get('/ninjas', (req, res) => {
+router.get('/ninjas', (req, res, next) => {
   res.send({ type: 'GET' });
 });
 
 // Add new ninja to db
-router.post('/ninjas', (req, res) => {
+router.post('/ninjas', (req, res, next) => {
   Ninja.create(req.body).then(ninja => {
     res.send(ninja);
-  });
+  }).catch(next);
 });
 
 // Update ninja in db
-router.put('/ninjas/:id', (req, res) => {
+router.put('/ninjas/:id', (req, res, next) => {
   res.send({ type: 'PUT' });
 });
 
 // Delete ninja from db
-router.delete('/ninjas/:id', (req, res) => {
+router.delete('/ninjas/:id', (req, res, next) => {
   res.send({ type: 'DELETE' });
 });
 
